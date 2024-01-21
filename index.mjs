ca0fda8129de5d73137e71cc3e667046445b2374
debaser9 <jason@jasonbraswell.com>
2024-01-21 11:32:47 -1000

    inital commit

--- index.mjs
+++ index.mjs
@@ -1,69 +1,35 @@
 // This lambda function queries Square the inventory quantities for a given array of ids.
 
 import { Client, Environment, ApiError } from "square";
+import CryptoJS from "crypto-js";
 
 export const handler = async (event, context, callback) => {
   let responseObject;
   let client;
-  let countsArray = [];
-  let calls = 1;
 
   try {
+    // Square setup
     if (!client)
       client = new Client({
         accessToken: process.env.SQUARE_ACCESS_TOKEN,
         environment: Environment.Production,
       });
-    const { inventoryApi } = client;
+    const { customersApi } = client;
+
+    // Get and decrypt email address
     const data = await JSON.parse(event.body);
-    const ids = data.ids;
-    const subArraysNeeded = Math.ceil(ids.length / 1000);
+    const encryptedEmailAddress = data.data;
+    const bytes = CryptoJS.AES.decrypt(
+      encryptedEmailAddress,
+      process.env.DECRYPT_KEY
+    );
+    const emailAddress = bytes.toString(CryptoJS.enc.Utf8);
 
-    for (let i = 0; i < subArraysNeeded; i++) {
-      let subArray;
-      if (i === subArraysNeeded - 1) {
-        subArray = ids.slice(1000 * i);
-      } else subArray = ids.slice(1000 * i, 1000 * (i + 1));
-
-      console.log(
-        `running subarray #${i} (${0 + 1000 * i}-${
-          i === subArraysNeeded - 1 ? null : 1000 * (i + 1)
-        })`
-      );
-
-      console.log(subArray);
-
-      console.log(`API call #${calls}`);
-      calls++;
-      const res = await inventoryApi.batchRetrieveInventoryCounts({
-        catalogObjectIds: subArray,
-        limit: 1000,
-      });
-      let { counts, cursor } = res.result;
-      counts.forEach((count) => {
-        countsArray.push(count);
-      });
-
-      while (cursor) {
-        console.log(`API call #${calls}`);
-        calls++;
-        console.log("cursor:", cursor);
-        const res = await inventoryApi.batchRetrieveInventoryCounts({
-          cursor,
-          catalogObjectIds: subArray,
-          limit: 1000,
-        });
-        counts = res.result.counts;
-        cursor = res.result.cursor;
-        counts.forEach((count) => {
-          countsArray.push(count);
-        });
-      }
-    }
+    // Create new customer with email address
+    const _ = await customersApi.createCustomer({ emailAddress });
 
     responseObject = {
       result: "success",
-      counts: countsArray,
     };
   } catch (error) {
     if (error instanceof ApiError) {
